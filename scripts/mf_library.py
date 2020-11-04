0acde0476b7e0e45c7b938544ddabbcbfc94c6ed
Guillaume DONVAL <guillaume.donval@fxinnovation.com>
2020-11-04 14:16:46 -0500

    feat: more code

--- scripts/mf_library.py
+++ scripts/mf_library.py
@@ -5,6 +5,7 @@ import logging
 import sys
 import yaml
 import os
+import re
 
 PATH_HOME=os.path.join(str(Path.home()), 'migration')
 PATH_TEMPLATE=os.path.join(PATH_HOME, 'templates')
@@ -17,36 +18,38 @@ DIRECTORY_POST_LAUNCH='post-launch'
 
 class DefaultsLoader:
     _defaults = {}
-    _logger = None
-
-    def __init__(self, logger):
-        self._logger = logger
 
     def load(self, default_config_file, environment):
         with open(default_config_file, 'r') as stream:
             try:
                 all_defaults = yaml.safe_load(stream)
                 self._defaults = all_defaults[environment]
-                self._logger.error(self._defaults)
+                logging.debug(self._defaults)
 
                 for environment, defaults in all_defaults.items():
-                    self._check_is_serializable_as_path(environment)
+                    Utils.check_is_serializable_as_path(environment)
                     self._environments.append(environment)
             except yaml.YAMLError as exception:
-                self._logger.error(exception)
+                logging.error(exception)
 
     def get(self):
         return self._defaults
 
-class Logger(logging.getLoggerClass()):
-    def __init__(self, verbose=False):
-        if verbose is True:
-            logging_level = logging.DEBUG
-        else:
-            logging_level = logging.INFO
+class Utils:
+    def check_is_serializable_as_path(self, string_to_test):
+        is_serializable_as_path = re.search("^[a-zA-Z0-9_-]+$", string_to_test)
+        if not is_serializable_as_path:
+            logging.warning('The string “'+string_to_test+'” will need to be serialized for a path or a filename. It might not be suitable because it contains special characters. Use at your own risk.')
+        return bool(is_serializable_as_path)
 
-        super().__init__()
-        super().basicConfig(stream=sys.stderr, level=logging_level)
+
+def setup_logging(verbose=False):
+    if verbose is True:
+        logging_level = logging.DEBUG
+    else:
+        logging_level = logging.INFO
+
+    logging.basicConfig(stream=sys.stderr, level=logging_level)
 
 
 if __name__ == '__main__':
