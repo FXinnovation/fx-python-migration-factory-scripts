f50f73617aed527cc07a6bf806e0e874f4a861ab
Guillaume DONVAL <guillaume.donval@fxinnovation.com>
2020-11-12 14:24:32 -0500

    feat: more code

--- scripts/mf_library.py
+++ scripts/mf_library.py
@@ -1,26 +1,28 @@
 #!/usr/bin/env python3
 
-from pathlib import Path
+import getpass
 import logging
-import sys
-import yaml
 import os
 import re
-import getpass
+import sys
+from pathlib import Path
+
 import boto3
+import yaml
 
-PATH_HOME=os.path.join(str(Path.home()), 'migration')
-PATH_TEMPLATE=os.path.join(PATH_HOME, 'templates')
+PATH_HOME = os.path.join(str(Path.home()), 'migration')
+PATH_TEMPLATE = os.path.join(PATH_HOME, 'templates')
 
-FILE_CSV_WAVE_TEMPLATE=os.path.join(PATH_TEMPLATE, 'migration-intake-form.csv')
-FILE_CSV_INIT_WAVE_TEMPLATE=os.path.join(PATH_TEMPLATE, 'migration-intake-form-init.csv')
-FILE_DONE_MARKER='.mf_done'
+FILE_CSV_WAVE_TEMPLATE = os.path.join(PATH_TEMPLATE, 'migration-intake-form.csv')
+FILE_CSV_INIT_WAVE_TEMPLATE = os.path.join(PATH_TEMPLATE, 'migration-intake-form-init.csv')
+FILE_DONE_MARKER = '.mf_done'
 
-DIRECTORY_POST_LAUNCH='post-launch'
+DIRECTORY_POST_LAUNCH = 'post-launch'
 
-ENV_VAR_AWS_ACCESS_KEY_NAMES=['MF_AWS_ACCESS_KEY_ID', 'AWS_ACCESS_KEY_ID', 'AWS_ACCESS_KEY']
-ENV_VAR_AWS_SECRET_KEY_NAMES=['MF_AWS_SECRET_ACCESS_KEY', 'AWS_SECRET_ACCESS_KEY', 'AWS_SECRET_KEY']
-ENV_VAR_AWS_REGION_NAMES=['MF_AWS_REGION', 'AWS_REGION']
+ENV_VAR_AWS_ACCESS_KEY_NAMES = ['MF_AWS_ACCESS_KEY_ID', 'AWS_ACCESS_KEY_ID', 'AWS_ACCESS_KEY']
+ENV_VAR_AWS_SECRET_KEY_NAMES = ['MF_AWS_SECRET_ACCESS_KEY', 'AWS_SECRET_ACCESS_KEY', 'AWS_SECRET_KEY']
+ENV_VAR_AWS_REGION_NAMES = ['MF_AWS_REGION', 'AWS_REGION']
+
 
 class DefaultsLoader:
     """ Loads default configuration values for every wave and fetch available environments """
@@ -34,7 +36,9 @@ class DefaultsLoader:
                 all_defaults = yaml.safe_load(stream)
 
                 if not environment in all_defaults:
-                    raise Exception('Environment “'+ environment +'” does not exists in “'+ default_config_file +'”.')
+                    raise Exception(
+                        'Environment “' + environment + '” does not exists in “' + default_config_file + '”.'
+                    )
 
                 self._defaults = all_defaults[environment]
 
@@ -55,13 +59,16 @@ class DefaultsLoader:
     def getAvailableEnvironments(self):
         return self._available_environments
 
+
 class Utils:
     """ Primitive type utilities """
 
     def check_is_serializable_as_path(self, string_to_test):
         is_serializable_as_path = re.search("^[a-zA-Z0-9_ -]+$", string_to_test)
         if not is_serializable_as_path:
-            logging.warning('The string “'+string_to_test+'” will need to be serialized for a path or a filename. It might not be suitable because it contains special characters. Use at your own risk.')
+            logging.warning(
+                'The string “' + string_to_test + '” will need to be serialized for a path or a filename. It might not be suitable because it contains special characters. Use at your own risk.'
+            )
         return bool(is_serializable_as_path)
 
 
@@ -70,7 +77,7 @@ class EnvironmentVariableFetcher():
 
     def fetch(self, env_var_names, env_var_description, sensitive=False):
         for env_var_name in env_var_names:
-            logging.debug(self.__class__.__name__ + ': Trying to fetch '+ env_var_name +' environment variable.')
+            logging.debug(self.__class__.__name__ + ': Trying to fetch ' + env_var_name + ' environment variable.')
 
             if env_var_name in os.environ:
                 return os.getenv(env_var_name)
@@ -79,6 +86,7 @@ class EnvironmentVariableFetcher():
             return getpass.getpass(env_var_description + ": ")
 
         return input(env_var_description + ": ")
+
 
 class AWSServiceAccessor():
     """ Login to AWS """
@@ -91,14 +99,22 @@ class AWSServiceAccessor():
 
     def __init__(self):
         self._environment_variable_fetcher = EnvironmentVariableFetcher()
-        self._aws_access_key = self._environment_variable_fetcher.fetch(ENV_VAR_AWS_ACCESS_KEY_NAMES, 'AWS Access Key ID')
-        self._aws_secret_access_key = self._environment_variable_fetcher.fetch(ENV_VAR_AWS_SECRET_KEY_NAMES, 'AWS Access Secret Key', sensitive=True)
+        self._aws_access_key = self._environment_variable_fetcher.fetch(
+            ENV_VAR_AWS_ACCESS_KEY_NAMES, 'AWS Access Key ID'
+        )
+        self._aws_secret_access_key = self._environment_variable_fetcher.fetch(
+            ENV_VAR_AWS_SECRET_KEY_NAMES, 'AWS Access Secret Key', sensitive=True
+        )
         self._aws_region = self._environment_variable_fetcher.fetch(ENV_VAR_AWS_REGION_NAMES, 'AWS Region')
-
 
     def get_ec2(self):
         if self._ec2_client is None:
-            self._ec2_client = boto3.client('ec2', aws_access_key_id=self._aws_access_key, aws_secret_access_key=self._aws_secret_access_key, region_name=self._aws_region)
+            self._ec2_client = boto3.client(
+                'ec2',
+                aws_access_key_id=self._aws_access_key,
+                aws_secret_access_key=self._aws_secret_access_key,
+                region_name=self._aws_region
+            )
 
         return self._ec2_client
 
