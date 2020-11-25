df25dfc8a462608b0546a145219c606a4c8ce786
Guillaume DONVAL <guillaume.donval@fxinnovation.com>
2020-11-24 20:26:46 -0500

    feat: (library) adds AWSValidator to validate common AWS resource names

--- scripts/mf/aws.py
+++ scripts/mf/aws.py
@@ -7,6 +7,7 @@ from . import ENV_VAR_AWS_REGION_NAMES
 from . import ENV_VAR_AWS_SECRET_KEY_NAMES
 from .utils import EnvironmentVariableFetcher
 
+import re
 
 class AWSServiceAccessor:
     """ Allows access to AWS API endpoints """
@@ -36,6 +37,36 @@ class AWSServiceAccessor:
             )
 
         return self._ec2_client
+
+
+class AWSValidator:
+    """ Check values to be compliant with AWS """
+
+    REGEXP_CLOUDENDURE_PROJECT_NAME = '^[0-9A-Za-z _-]{2,64}$'
+    REGEXP_ACCOUNT_ID = '^[0-9]{12}$'
+    REGEXP_SUBNET_ID = '^subnet-([a-z0-9]{8}|[a-z0-9]{17})$'
+    REGEXP_SECURITY_GROUP_ID = '^sg-([a-z0-9]{8}|[a-z0-9]{17})$'
+    REGEXP_INSTANCE_TYPE = '^(u-)?[a-z0-9]{2,4}\\.(nano|micro|small|medium|metal|(2|4|8|16|24)?x?large)$'
+
+    @classmethod
+    def is_account_id(cls, value):
+        return re.match(cls.REGEXP_ACCOUNT_ID, value)
+
+    @classmethod
+    def is_cloud_endure_project_name(cls, value):
+        return re.match(cls.REGEXP_WAVE_NAME, value)
+
+    @classmethod
+    def is_subnet_id(cls, value):
+        return re.match(cls.REGEXP_SUBNET_ID, value)
+
+    @classmethod
+    def is_security_group_id(cls, value):
+        return re.match(cls.REGEXP_SECURITY_GROUP_ID, value)
+
+    @classmethod
+    def is_instance_type(cls, value):
+        return re.match(cls.REGEXP_INSTANCE_TYPE, value)
 
 
 if __name__ == '__main__':
